55808a0a4df95e43a2b8070ffc147d5dea99a854
A. Dugue <76267790+adug-git@users.noreply.github.com>
2021-09-06 12:56:55 -0400

    Add testatexit.c + testatexit

---
+++ testatexit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cleanup() {
+    printf("Thank you\n");
+}
+
+
+int main (int argc, char **argv) 
+{
+    atexit(cleanup);
+    
+    if (argc != 2)
+    {
+        printf("usage: %s <number>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    int value = atoi(argv[1]);
+    printf("%d^2 == %d\n", value, value * value);
+    
+    return EXIT_SUCCESS;
+}
